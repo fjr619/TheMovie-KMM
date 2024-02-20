3334357618c551706cc647783bac72f64805c816
Franky Wijanarko <frank.jr.619@gmail.com>
2024-02-20 15:59:04 +0700

    sql delight part 2 : implement insert movie

--- shared/src/commonMain/kotlin/com/fjr619/themovie_kmm/data/repository/local/datasource/MovieLocalDataSource.kt
+++ shared/src/commonMain/kotlin/com/fjr619/themovie_kmm/data/repository/local/datasource/MovieLocalDataSource.kt
@@ -3,5 +3,6 @@ package com.fjr619.themovie_kmm.data.repository.local.datasource
 import com.fjr619.themovie_kmm.domain.entity.Movie
 
 interface MovieListLocalDataSource {
+    fun insertMovieListToDB(movieList: List<Movie>)
     fun getPopularMovieListFromLocal(): List<Movie>
 }
--- shared/src/commonMain/kotlin/com/fjr619/themovie_kmm/data/repository/local/datasourceimpl/MovieLocalDataSourceImpl.kt
+++ shared/src/commonMain/kotlin/com/fjr619/themovie_kmm/data/repository/local/datasourceimpl/MovieLocalDataSourceImpl.kt
@@ -9,6 +9,20 @@ class MovieLocalDataSourceImpl(
     private val movieDatabase: MovieDatabase
 ): MovieListLocalDataSource {
     private val queries = movieDatabase.movieDatabaseQueries
+    override fun insertMovieListToDB(movieList: List<Movie>) {
+        for (movieItem in movieList) {
+            queries.transaction {
+                queries.insertMovielist(
+                    id = movieItem.id,
+                    title = movieItem.title,
+                    overview = movieItem.overview,
+                    popularity = movieItem.popularity.toLong(),
+                    poster_path = movieItem.poster_path
+                )
+            }
+        }
+    }
+
     override fun getPopularMovieListFromLocal(): List<Movie> {
         return queries.getMovielist(mapper = ::asDomainMovieEntity).executeAsList()
     }
